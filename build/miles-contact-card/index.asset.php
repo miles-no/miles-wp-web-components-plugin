d1242e48d7785b8217a13459bc7488f0d14b5fec
Ronny Myhre Njaastad <ronny@njaastad.no>
2023-06-16 11:18:37 +0200

    fix: css for contact card

--- build/miles-contact-card/index.asset.php
+++ build/miles-contact-card/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '935c3216cb247c08fe3f');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '9847f42bcfc6cfdcd7d9');
