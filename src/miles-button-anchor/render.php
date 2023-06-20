31a5b208359e72079df4c0d4cfc94559b0a0ce75
Ronny Myhre Njaastad <ronny@njaastad.no>
2023-06-20 09:00:10 +0200

    fix: button-anchor correct order for needle and haystack on anchor check

--- src/miles-button-anchor/render.php
+++ src/miles-button-anchor/render.php
@@ -4,7 +4,7 @@ $content = $attributes['content'] ?? '';
 <miles-button-anchor color="#3F1221" <?php
 foreach ($attributes as $key=>$value){
 	if ($key == 'href') {
-		$value = str_starts_with('#', $value) ? $value : '#' . $value;
+		$value = (str_starts_with($value, '#')) ? $value : '#' . $value;
 	}
 	echo $key . '="' . $value .'" ';
 }
