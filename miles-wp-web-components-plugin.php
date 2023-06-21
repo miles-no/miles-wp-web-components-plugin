897f0e5d5d96d0f2876f54dc98badf9cb7c08846
Ronny Myhre Njaastad <ronny@njaastad.no>
2023-06-21 18:35:07 +0200

    feat: added miles-banner

--- miles-wp-web-components-plugin.php
+++ miles-wp-web-components-plugin.php
@@ -22,11 +22,12 @@
  */
 function create_block_miles_editor_blocks_block_init(): void
 {
+	register_block_type( __DIR__ . '/build/miles-banner' );
 	register_block_type( __DIR__ . '/build/miles-business-card' );
-	register_block_type( __DIR__ . '/build/miles-contact-card' );
 	register_block_type( __DIR__ . '/build/miles-button-anchor' );
-	register_block_type( __DIR__ . '/build/miles-office-banner' );
+	register_block_type( __DIR__ . '/build/miles-contact-card' );
 	register_block_type( __DIR__ . '/build/miles-info-block' );
+	register_block_type( __DIR__ . '/build/miles-office-banner' );
 
 }
 add_action( 'init', 'create_block_miles_editor_blocks_block_init' );
