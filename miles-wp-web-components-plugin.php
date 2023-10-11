077c9dc1efdb49819666db623e7ff8529d9cbf0a
Knut Hufthammer <knut.hufthammer@miles.no>
2023-10-11 13:20:47 +0200

    #5 added fagblogg teaser block

--- miles-wp-web-components-plugin.php
+++ miles-wp-web-components-plugin.php
@@ -29,6 +29,7 @@ function create_block_miles_editor_blocks_block_init(): void {
 	register_block_type( __DIR__ . '/build/miles-contact-card' );
 	register_block_type( __DIR__ . '/build/miles-image-block' );
 	register_block_type( __DIR__ . '/build/miles-office-banner' );
+	register_block_type( __DIR__ . '/build/miles-fagblogg-teaser' );
 
 }
 
