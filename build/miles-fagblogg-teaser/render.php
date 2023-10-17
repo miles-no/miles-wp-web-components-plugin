d325a0c9f21f060664773c2895036d44269a4b37
Knut Hufthammer <knut.hufthammer@miles.no>
2023-10-17 10:41:50 +0200

    added missing build files for fagblokk-teaser

---
+++ build/miles-fagblogg-teaser/render.php
@@ -0,0 +1,22 @@
+
+<?php
+
+    $attributestring = '';
+
+    foreach ($attributes as $key => $value) {
+        $attributestring .= $key . '="' . esc_attr($value) . '" ';
+    }
+
+    // latest post block
+    $numberOfPosts = isset($attributes['numberOfPosts']) ? intval($attributes['numberOfPosts']) : 3;
+    
+	$latestPostsBlock = '<!-- wp:latest-posts {"postsToShow":' . $numberOfPosts . ', "displayAuthor":' . ($attributes['displayAuthor'] ? 'true' : 'false') . ', "displayPostDate":' . ($attributes['displayPostDate'] ? 'true' : 'false') . ', "displayFeaturedImage":' . ($attributes['displayFeaturedImage'] ? 'true' : 'false') . '} /-->';
+
+    $parsedPosts = do_blocks($latestPostsBlock);
+
+    $output = '<miles-fagblogg-teaser ' . $attributestring . '>';
+    $output .= $parsedPosts;
+    $output .= '</miles-fagblogg-teaser>';
+
+    echo $output;
+
